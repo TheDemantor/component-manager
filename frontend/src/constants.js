d2c0c3aafe74a9d0d135f52f4a0ac9e90ad38b97
Your Name Here <your@email.example>
2023-09-29 13:23:57 +0530

    Added production server2

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NODE_ENV === 'development'? 'http://localhost:5000' : '';
+export const BASE_URL = process.env.NODE_ENV === 'development'? 'http://localhost:5000' : 'https://component-manager.onrender.com';
 export const COMPONENT_URL  = '/api/components';
